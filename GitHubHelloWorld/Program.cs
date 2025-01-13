eaee905fe3a147fc823e37e1909db8af392e4081
Camelia Ciocan <camelia-carmen.ciocan@oebb.at>
2025-01-13 08:02:28 +0100

    Projektdateien hinzufügen.

---
+++ GitHubHelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitHubHelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
